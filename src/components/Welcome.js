b0c106003bb74375f3d3b938a3efcb3547ecaa3e
gemgupta <gemgupta007@gmail.com>
2023-10-05 23:39:01 +0530

    verify email

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import AuthContext from "./Store/Auth-Context";
+import { useContext } from "react";
 function Welcome() {
+  const Authctx = useContext(AuthContext);
+  const token = Authctx.Token;
+  const verifyUserHandler = async () => {
+    try {
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCybdrpqrvY0IcG00qrSUGktX-0TbtpEok`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: token,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className=""> 
+    <div className="">
       <h1 className=" block text-xl">Welcome to expense tracker</h1>
-      <div  className="flex justify-end p-3">
+      <div className="flex justify-end p-3">
         Your profile is incomplete{" "}
         <Link className="underline text-blue-600" to="/Details">
           Click here to complete
         </Link>{" "}
       </div>
+      <h2 className="underline text-blue-600" onClick={verifyUserHandler}> Verify your email</h2>
     </div>
   );
 }
